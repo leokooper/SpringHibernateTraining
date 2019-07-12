e4696f98c1f28202ff60e9e7d936c8fc03fceb07
Leo Kooper <lverixas@gmial.com>
2019-07-13 00:05:22 +0300

    //ServletInitialiser edited application.yml added

--- spring-hibernate-demo/src/main/java/ru/leonchenko/spring/sprinthibernate/ServletInitializer.java
+++ spring-hibernate-demo/src/main/java/ru/leonchenko/spring/sprinthibernate/ServletInitializer.java
@@ -1,13 +1,13 @@
 package ru.leonchenko.spring.sprinthibernate;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+@ComponentScan(basePackages = {"ru.leonchenko.spring.sprinthibernate"})
 public class ServletInitializer extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SprintHibernateDemoProjectApplication.class);
     }
 
-}
