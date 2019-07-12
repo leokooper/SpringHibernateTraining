e007bdd31e37d18f510277220034234201cfaa7c
Leo Kooper <lverixas@gmial.com>
2019-07-12 23:21:32 +0300

    //spring-hibernate project start

---
+++ spring-hibernate-demo/src/main/java/ru/leonchenko/spring/sprinthibernate/SprintHibernateDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.leonchenko.spring.sprinthibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintHibernateDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintHibernateDemoProjectApplication.class, args);
+    }
+
+}
