9c4757f1f3b431ae2bcf33619efd86a407b031b4
Verixas <lverixas@gmail.com>
2019-10-03 00:09:53 +0300

    //start simple String app

---
+++ chop-chop-sushi/src/main/java/ru/leonchenko/spring/chopchopsushi/ChopChopSushiApplication.java
@@ -0,0 +1,13 @@
+package ru.leonchenko.spring.chopchopsushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopChopSushiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChopChopSushiApplication.class, args);
+    }
+
+}
---
+++ chop-chop-sushi/src/test/java/ru/leonchenko/spring/chopchopsushi/HomePageControllerTest.java
@@ -0,0 +1,39 @@
+package ru.leonchenko.spring.chopchopsushi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+
+/**
+ * @author Igor Leonchenko
+ * @version 1.0
+ */
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomePageControllerTest.class)
+public class HomePageControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Home Page"))
+                .andExpect(content().string(containsString("Welcome to Chop Chop Sushi!")));
+    }
+}
