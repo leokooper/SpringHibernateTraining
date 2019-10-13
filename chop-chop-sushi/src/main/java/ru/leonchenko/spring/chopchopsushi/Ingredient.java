38ba9413b2e3932380102053ad832333c1402552
Verixas <lverixas@gmail.com>
2019-10-13 16:08:01 +0300

    //added Hibernate JPA Repository

--- chop-chop-sushi/src/main/java/ru/leonchenko/spring/chopchopsushi/Ingredient.java
+++ chop-chop-sushi/src/main/java/ru/leonchenko/spring/chopchopsushi/Ingredient.java
@@ -1,7 +1,12 @@
 package ru.leonchenko.spring.chopchopsushi;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * @author Igor Leonchenko
@@ -10,8 +15,11 @@ import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
-  
+
+  @Id
   private final String id;
   private final String name;
   private final Type type;
